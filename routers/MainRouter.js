8429ab7660be74d094650538deec1d2b90e80661
Lance Batson <lancebatsondev@gmail.com>
2014-11-07 09:29:29 -0600

    working on moving test page into router

--- routers/MainRouter.js
+++ routers/MainRouter.js
@@ -1,23 +1,29 @@
 App.Routers = App.Routers || {};
 
 (function(){
-    var router = Backbone.Router.extend({
-        routes: {
-            ''      : 'index',
-            'tests' : 'tests'
-        },
-        initialize : function (){
-            Backbone.history.start();
-        },
-        index: function(){
-            $('.view-container').empty();
-            $('.view-container').html(Handlebars.compile($('#MainView').html()));
-            bindEvents();
-        },
-        tests: function(){
-            $('.view-container').empty();
-            $('.view-container').html(Handlebars.compile($('#TestView').html()));
-        }
-    });
+    var $container = $('.view-container'),
+        router = Backbone.Router.extend({
+            routes: {
+                ''      : 'index',
+                'tests' : 'tests'
+            },
+            initialize : function (){
+                Backbone.history.start();
+            },
+            index: function(){
+                $container.empty();
+                $container.html(Handlebars.compile($('#MainView').html()));
+                bindEvents();
+            },
+            tests: function(){
+                $container.empty();
+                $container.html(Handlebars.compile($('#TestView').html()));
+                mocha.setup('bdd');
+                runTests();
+                mocha.checkLeaks();
+                mocha.globals(['jQuery']);
+                mocha.run();
+            }
+        });
     App.Routers.MainRouter = new router();
 })();
--- tests/script_test.js
+++ tests/script_test.js
@@ -1,67 +1,67 @@
 
-var expect = chai.expect;
+function runTests() {
+    var expect = chai.expect;
 
-describe("The App", function() {
+    describe("The App", function() {
+        it("App exists", function() {
+          expect(App).to.exist;
+        });
 
-    it("App exists", function() {
-      expect(App).to.exist;
+        it("App contains 'Views, Models, Routers, and Collections'", function(){
+          expect(App).to.include.keys(
+            "Collections",
+            "Models",
+            "Views",
+            "Routers"
+          );
+        });
     });
 
-    it("App contains 'Views, Models, Routers, and Collections'", function(){
-      expect(App).to.include.keys(
-        "Collections",
-        "Models",
-        "Views",
-        "Routers"
-      );
-    })
-
-});
-
-describe("Test Model", function(){
-  it("Model should have defaults", function(){
-    var model = new App.Models.TestModel();
-    expect(model).to.be.ok;
-    expect(model.get('title')).to.equal("Test Model");
-    expect(model.get('description')).to.equal("This is a test model");
-  })
-})
-
-describe("Base View", function() {
-    it("BaseView should be an object", function() {
-      var list = new App.Views.BaseView();
-      // expect(list).to.be.an("object");
-      expect(list).to.be.an("object");
-    });
-});
-
-describe("Base View List", function() {
-    // var sandbox;
-    //
-    // beforeEach(function() {
-    //   // create a sandbox
-    //   sandbox = sinon.sandbox.create();
-    //
-    //   // stub some console methods
-    //   sandbox.stub(window.console, "log");
-    //   sandbox.stub(window.console, "error");
-    // });
-    //
-    // afterEach(function() {
-    //   // restore the environment as it was before
-    //   sandbox.restore();
-    // });
-    //var model
-
-    it("Base List View should be an object", function() {
-      console.log(App.Views);
-      var tests = {};
-      var list = new App.Views.ListView();
-
-      expect(list).to.be.an("object");
+    describe("Test Model", function(){
+        it("Model should have defaults", function(){
+            var model = new App.Models.TestModel();
+            expect(model).to.be.ok;
+            expect(model.get('title')).to.equal("Test Model");
+            expect(model.get('description')).to.equal("This is a test model");
+        });
     });
 
-    // it("Mode should trigger an add event"){
-    //
-    // }
-});
+    describe("Base View", function() {
+        it("BaseView should be an object", function() {
+            var list = new App.Views.BaseView();
+            // expect(list).to.be.an("object");
+            expect(list).to.be.an("object");
+        });
+    });
+
+    describe("Base View List", function() {
+        // var sandbox;
+        //
+        // beforeEach(function() {
+        //   // create a sandbox
+        //   sandbox = sinon.sandbox.create();
+        //
+        //   // stub some console methods
+        //   sandbox.stub(window.console, "log");
+        //   sandbox.stub(window.console, "error");
+        // });
+        //
+        // afterEach(function() {
+        //   // restore the environment as it was before
+        //   sandbox.restore();
+        // });
+        //var model
+
+        it("Base List View should be an object", function() {
+          console.log(App.Views);
+          var tests = {};
+          var list = new App.Views.ListView();
+
+          expect(list).to.be.an("object");
+        });
+
+        // it("Mode should trigger an add event"){
+        //
+        // }
+    });
+}
