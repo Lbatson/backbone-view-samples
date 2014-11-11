e4f48ec6a0709b226294021d5de91856daedfee9
Lance Batson <lancebatsondev@gmail.com>
2014-11-11 14:32:45 -0600

    added test for model capture and revert

--- test/spec/test.js
+++ test/spec/test.js
@@ -17,21 +17,52 @@
     });
 
     describe('Base Model', function(){
-        it('Model should have defaults', function(){
+        it('model should have defaults', function(){
             var model = new App.Models.BaseModel();
             expect(model).to.be.ok;
             expect(model.get('title')).to.equal('Test Model');
             expect(model.get('description')).to.equal('This is a test model');
         });
+
+        it('model.set() should update value and get() should return new value', function(){
+            var model = new App.Models.BaseModel();
+            model.set('title', 'TEST');
+            expect(model.get('title')).to.equal('TEST');
+        });
+
+        it('model.capture() should create model._original', function(){
+            var model = new App.Models.BaseModel();
+            model.capture();
+            expect(model._original).to.be.an('object');
+        });
+
+        it('model._original should have starting attributes from when created', function(){
+            var model = new App.Models.BaseModel();
+            model.capture();
+            expect(model._original.title).to.equal(model.get('title'));
+            expect(model._original.description).to.equal(model.get('description'));
+        });
+
+        it('model.revert() should set model back to model._original values', function(){
+            var model = new App.Models.BaseModel();
+            model.capture();
+            model.set('title', 'TEST');
+            model.set('description', 'TEST');
+            expect(model.get('title')).to.equal('TEST');
+            expect(model.get('description')).to.equal('TEST');
+            model.revert();
+            expect(model.get('title')).to.equal('Test Model');
+            expect(model.get('description')).to.equal('This is a test model');
+        });
     });
 
-    describe('Base View', function() {
-        it('BaseView should be an object', function() {
+    describe('BaseView', function() {
+        it('base should be an object', function() {
             expect(new App.Views.Base()).to.be.an('object');
         });
     });
 
-    describe('Base View List', function() {
+    describe('BaseList', function() {
         // var sandbox;
         //
         // beforeEach(function() {
