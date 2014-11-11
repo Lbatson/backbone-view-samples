51102f9452553344df01c33eaa7108c0970d6970
Lance Batson <lancebatsondev@gmail.com>
2014-11-11 13:56:55 -0600

    update tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -5,19 +5,20 @@
             expect(App).to.exist;
         });
 
-        it('App contains Views, Models, Routers, and Collections', function(){
+        it('App contains Views, Models, Controllers, Routers, and Collections', function(){
             expect(App).to.include.keys(
                 'Collections',
                 'Models',
+                'Controllers',
                 'Views',
                 'Routers'
             );
         });
     });
 
-    describe('Test Model', function(){
+    describe('Base Model', function(){
         it('Model should have defaults', function(){
-            var model = new App.Models.TestModel();
+            var model = new App.Models.BaseModel();
             expect(model).to.be.ok;
             expect(model.get('title')).to.equal('Test Model');
             expect(model.get('description')).to.equal('This is a test model');
@@ -26,9 +27,7 @@
 
     describe('Base View', function() {
         it('BaseView should be an object', function() {
-            var list = new App.Views.BaseView();
-            // expect(list).to.be.an("object");
-            expect(list).to.be.an('object');
+            expect(new App.Views.Base()).to.be.an('object');
         });
     });
 
@@ -51,9 +50,7 @@
         //var model
 
         it('Base List View should be an object', function() {
-            var tests = 'test';
-            var list = new App.Views.ListView();
-            expect(tests).to.be.an('object');
+            expect(new App.Views.BaseList()).to.be.an('object');
         });
 
         // it("Mode should trigger an add event"){
